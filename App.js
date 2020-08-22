76f78a521de6563ab621487567927b38bd63aef8
Judlup Luns <judlup@gmail.com>
2020-08-22 11:26:50 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Main from './app/components/Main';
+
+export default function App() {
+  return (
+    <Main />
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
